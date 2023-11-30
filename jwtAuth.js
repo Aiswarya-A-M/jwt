6a68e1a25a2bef20cef5d38540e766277e6a5f26
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-30 16:24:43 +0530

    formatted

--- jwtAuth.js
+++ jwtAuth.js
@@ -3,10 +3,10 @@ const express = require("express");
 const dotEnv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+
 dotEnv.config();
 const app = express();
 app.use(express.json());
-let refreshTokens = [];
 
 const connection = mysql.createConnection({
   host: process.env.host,
@@ -41,7 +41,7 @@ app.post("/register", (req, res) => {
             return console.log(error);
           }
         });
-        res.end("added to registration table");
+        return res.end("added to registration table");
       }
       return res.end("given mail id is already used");
     });
@@ -70,15 +70,15 @@ app.post("/login", (req, res) => {
         if (isMatch) {
           const user = { id: result[0].id };
           const accessToken = generateAccessToken(user);
-          const refreshToken = jwt.sign(
-            user,
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: "3600s" }
-          );
-          refreshTokens.push(refreshToken);
+          const refreshToken = generateRefreshToken(user);
+          res.cookie("refreshToken", refreshToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: "strict",
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+          });
           return res.json({
             accessToken: accessToken,
-            refreshToken: refreshToken,
           });
         }
         return res.send("invalid password");
@@ -89,35 +89,35 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.post("/token", (req, res) => {
-  const refreshToken = req.body.token;
-  if (!refreshToken) {
-    res.send("no refresh token");
-  }
-  if (!refreshTokens.includes(refreshToken)) {
-    return res.send("invalid refresh token");
-  }
-  jwt.verify(
-    refreshToken,
-    process.env.REFRESH_TOKEN_SECRET,
-    (error, result) => {
-      console.log(result);
-      const accessToken = generateAccessToken({
-        userId: result.id,
+app.get("/token", (req, res) => {
+  const cookie = req.headers.cookie.split("; ")[0];
+  const refreshToken = cookie.split("=")[1];
+  if (refreshToken) {
+    jwt.verify(refreshToken, process.env.SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ error: "Invalid or expired refresh token" });
+      }
+      const newAccessToken = generateAccessToken({ userId: decoded.userId });
+      const newRefreshToken = generateRefreshToken({ userId: decoded.userId });
+      res.cookie("refreshToken", newRefreshToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       });
-      res.send({ "new token": accessToken });
-    }
-  );
-});
 
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "300s" });
-}
+      return res.json({ newAccessToken: newAccessToken });
+    });
+  }
+  return res.status(401).json({ error: "Refresh token not found" });
+});
 
 app.get("/user/:id", authenticateToken, (req, res) => {
   try {
     const id = req.params.id;
-    const sql = `SELECT * FROM registration WHERE id= ${id}`;
+    const sql = `SELECT fname,lname,phoneNumber FROM registration WHERE id= ${id}`;
     connection.query(sql, (error, result) => {
       if (error) {
         return console.log(error);
@@ -180,15 +180,20 @@ app.delete("/user/:id", authenticateToken, (req, res) => {
   }
 });
 
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "30s" });
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "86400s" });
+}
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  console.log(authHeader);
   const token = authHeader.split(" ")[1];
-  console.log(token);
   if (!token) {
     return res.sendStatus(403);
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, result) => {
+  jwt.verify(token, process.env.SECRET_KEY, (error, result) => {
     if (error) {
       return res.end("invalid token");
     }
